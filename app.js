642a65c95cfb3644f8ff41006ae5c9016ca4ebd4
John Wood <jwood@financeit.io>
2021-12-23 16:43:58 -0500

    Added node cli for connecting, creating wallets, checking balance, and sending funds.

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 const xrpl = require("xrpl")
 
-async function main() {
+let client = null
 
-  // Define the network client
-  const PUBLIC_TEST_SERVER = "wss://s.altnet.rippletest.net:51233"
-  const client = new xrpl.Client(PUBLIC_TEST_SERVER)
-  await client.connect()
+async function connect() {
+  try {
+    const PUBLIC_TEST_SERVER = "wss://s.altnet.rippletest.net:51233"
+    client = new xrpl.Client(PUBLIC_TEST_SERVER)
+    const response = await client.connect()
+    console.log(`Connected to ${client.connection.url}`)
+  } catch {
+    console.log("Could not connect to test server")
+  }
+}
 
-  let response = await client.request({
-    "command": "ledger",
-    "ledger_index": "validated",
-    "transactions": true
-  });
-  console.log(response);
-
-  // Disconnect when done (If you omit this, Node.js won't end the process)
+async function disconnect() {
   client.disconnect()
 }
 
-main()
+async function createAndFundWallet() {
+  let response = await client.fundWallet()
+  console.log(response)
+  return response.wallet
+}
+
+async function getBalance(wallet) {
+  const response = await client.request({
+    "command": "account_info",
+    "account": wallet.address,
+    "ledger_index": "validated"
+  })
+  console.log(`Wallet balance is ${response.result.account_data.Balance}`)
+}
+
+async function sendFunds(wallet1, amount, wallet2) {
+  const prepared = await client.autofill({
+    "TransactionType": "Payment",
+    "Account": wallet1.address,
+    "Amount": amount,
+    "Destination": wallet2.address
+  })
+
+  const max_ledger = prepared.LastLedgerSequence
+  console.log("Prepared transaction details:", prepared)
+
+  signed = wallet1.sign(prepared)
+  const tx = await client.submitAndWait(signed.tx_blob)
+  console.log(tx)
+}
